e3eb962ae9cc96c88b3172b2321a4b7cc3ae3635
Kristennn0420 <60125845+Kristennn0420@users.noreply.github.com>
2026-04-06 15:29:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World Kristen"
+    return "Version 2 - deployed by GitHub Actions"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
